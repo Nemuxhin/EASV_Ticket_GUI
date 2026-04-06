6d1f4f60c5870d2a872d0f1a1561260344ec3904
Nemuxhin <Nemuxhin@gmail.com>
2026-04-06 21:26:12 +0200

    Delete src directory

--- src/Java/Be/User.java
+++
@@ -1,6 +0,0 @@
-package Java.Be;
-public class User {
-    private String username, role;
-    public User(String u, String r) { username = u; role = r; }
-    public String getRole() { return role; }
-}
